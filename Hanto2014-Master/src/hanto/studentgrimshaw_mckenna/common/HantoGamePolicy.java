dbd4549042ca9e1791cd0ef9b0fa7836b72b9272
magwitch324 <magwitch324@gmail.com>
2014-09-15 19:25:08 -0400

    renamed files

--- Hanto2014-Master/src/hanto/studentgrimshaw_mckenna/common/HantoGamePolicy.java
+++
@@ -1,9 +0,0 @@
-package hanto.studentgrimshaw_mckenna.common;
-
-import hanto.common.HantoPieceType;
-
-import java.util.Map;
-
-public interface HantoGamePolicy {
-	Map<HantoPieceType, Integer> getStartingHand();
-}
